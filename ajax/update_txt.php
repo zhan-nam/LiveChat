6d82bce66388ca30856e26c8738cda1a70e822dc
zhan-nam <61382190+zhan-nam@users.noreply.github.com>
2020-05-18 06:33:06 +0800

    ''

--- ajax/update_txt.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connect.php");
-$result = $conn -> query("select count(from_user) total_val from message");
-$row = $result->fetch_assoc();
-
-$myfile = fopen("update.txt", "w");
-fwrite($myfile, $row["total_val"]);
-fclose($myfile);
-?>
--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user = "root";
-$host = "localhost";
-$pass = "";
-$db = "livechat";
-$conn = mysqli_connect($host,$user,$pass,$db);
-?>
